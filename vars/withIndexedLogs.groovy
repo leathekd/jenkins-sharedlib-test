7c2ddfcc17a7fd97bd4c56581e9cf6f19f0c76ca
David Leatherman <git@david.leatherman.fm>
2018-02-01 11:10:28 -0500

    Add the first line to the output to be reallllly sure

--- vars/withIndexedLogs.groovy
+++ vars/withIndexedLogs.groovy
@@ -3,7 +3,11 @@ def call(Closure body) {
         body()
     }
     finally {
+        sh "echo FINALLY"
+        def firstLine
         def logfile = currentBuild.rawBuild.getLogFile()
+        logfile.withReader { firstLine = it.readLine() }
         sh "echo ${logfile.getAbsolutePath()}"
+        sh "echo \"${firstLine}\""
     }
 }
