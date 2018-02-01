d8c420e3e97e478ab44d7d4ea73b53c88dd86beb
David Leatherman <git@david.leatherman.fm>
2018-02-01 17:06:15 -0500

    wip - thinking out loud

--- vars/withIndexedLogs.groovy
+++ vars/withIndexedLogs.groovy
@@ -1,11 +1,40 @@
-def call(Closure body) {
+def existsInPath(String exec) {
+    boolean existsInPath = Stream.of(System.getenv("PATH")
+                                     .split(Pattern.quote(File.pathSeparator)))
+        .map(Paths::get)
+        .anyMatch(path -> Files.exists(path.resolve(exec)));
+    return existsInPath
+}
+
+def runbld(String... args) {
     try {
-        body()
+        if(existsInPath("runbld")) {
+            def runbldProc = "runbld ${args.join(" ")}".execute()
+            if( 0 == runbldProc.exitValue() ) {
+                return runbldProc.text
+            } else {
+                // TODO: print the exit code, stderr/stdout
+                
+            }
+        }
+    } catch(ex) {
+        // Don't let runbld kill the build
+        // TODO: print debug info
     }
-    finally {
+}
+
+def call(Closure body) {
+    def buildId = runbld("--generateId")
+    try {
+        // log that the job started
+        runbld("--build-id", buildId, "--event" "build-start")
+        body()
+    } finally {
         sh "echo FINALLY"
         def logfile = currentBuild.rawBuild.getLogFile()
-        sh "echo ${logfile.getAbsolutePath()}"
-        sh "echo ${currentBuild.rawBuild.getLogReader().readLine()}"
+        runbld("--build-id", buildId,
+               "--event", "build-end",
+               "--result", currentBuild.currentResult,
+               "--log-file", logfile.getAbsolutePath())
     }
 }
