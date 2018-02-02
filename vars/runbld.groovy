276f11c3c794c2b65b322c452e6211b9cf293e72
David Leatherman <git@david.leatherman.fm>
2018-02-02 12:26:50 -0500

    maybe @Field will work?

--- vars/runbld.groovy
+++ vars/runbld.groovy
@@ -1,20 +1,29 @@
 
 // dunno if this is allowed or will work cross-node
 
-def buildId() {
-    return (new co.elastic.Runbld()).buildId
+@Field
+String bld = new co.elastic.Runbld()
+
+def recordStart() {
+    stage ('Record build start with runbld') {
+        steps {
+            script {
+                bld.call("--event", "build-start")
+            }
+        }
+    }
 }
 
-def recordStart(id) {
-    def bld = new co.elastic.Runbld(id)
-    bld.call("--event", "build-start")
-}
-
-def recordEnd(id) {
-    def bld = new co.elastic.Runbld(id)
-    def logfile = currentBuild.rawBuild.getLogFile()
-    bld.call("--cwd", env.WORKSPACE,
-                "--event", "build-end",
-                "--result", currentBuild.currentResult,
-                "--log-file", logfile.getAbsolutePath())
+def recordEnd() {
+    stage ('Record build done with runbld') {
+        steps {
+            script {
+                def logfile = currentBuild.rawBuild.getLogFile()
+                bld.call("--cwd", env.WORKSPACE,
+                         "--event", "build-end",
+                         "--result", currentBuild.currentResult,
+                         "--log-file", logfile.getAbsolutePath())
+            }
+        }
+    }
 }
