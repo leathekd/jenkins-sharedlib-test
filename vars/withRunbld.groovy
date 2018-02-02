6e4e342211aa98e7718706bf56219de2fe29dff8
David Leatherman <git@david.leatherman.fm>
2018-02-02 15:30:06 -0500

    update to use static methods

--- vars/withRunbld.groovy
+++ vars/withRunbld.groovy
@@ -6,15 +6,15 @@ def call(Closure body) {
     def buildId = co.elastic.Runbld.randomId()
     try {
         // log that the job started
-        runbld.call(buildId, "--event", "build-start")
+        co.elastic.Runbld.call(buildId, "--event", "build-start")
         body()
     } finally {
         println "echo FINALLY"
         def logfile = currentBuild.rawBuild.getLogFile()
-        runbld.call(buildId,
-                    "--cwd", env.WORKSPACE,
-                    "--event", "build-end",
-                    "--result", currentBuild.currentResult,
-                    "--log-file", logfile.getAbsolutePath())
+        co.elastic.Runbld.call(buildId,
+                               "--cwd", env.WORKSPACE,
+                               "--event", "build-end",
+                               "--result", currentBuild.currentResult,
+                               "--log-file", logfile.getAbsolutePath())
     }
 }
