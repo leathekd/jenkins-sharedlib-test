65e9ac45c03275ea4be6041ee102a58ff78af6e7
David Leatherman <git@david.leatherman.fm>
2018-02-02 11:58:27 -0500

    trying to figure out what property is missing

--- vars/runbld.groovy
+++ vars/runbld.groovy
@@ -1,19 +1,19 @@
 
 // dunno if this is allowed or will work cross-node
 
-def runbld = new co.elastic.Runbld()
+def bld = new co.elastic.Runbld()
 
 def buildId() {
-    runbld.buildId
+    bld.buildId
 }
 
 def recordStart() {
-    runbld.call("--event", "build-start")
+    bld.call("--event", "build-start")
 }
 
 def recordEnd() {
     def logfile = currentBuild.rawBuild.getLogFile()
-    runbld.call("--cwd", env.WORKSPACE,
+    bld.call("--cwd", env.WORKSPACE,
                 "--event", "build-end",
                 "--result", currentBuild.currentResult,
                 "--log-file", logfile.getAbsolutePath())
