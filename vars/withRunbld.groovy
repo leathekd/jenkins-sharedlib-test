43c041fa54e38606e69e1fa2e552e6b9e025b723
David Leatherman <git@david.leatherman.fm>
2018-02-02 08:44:17 -0500

    rework finding runbld on the path, rename file

--- vars/withRunbld.groovy
+++ vars/withRunbld.groovy
@@ -1,21 +1,25 @@
-def existsInPath(String exec) {
-    boolean existsInPath = Stream.of(System.getenv("PATH")
-                                     .split(Pattern.quote(File.pathSeparator)))
-        .map(Paths::get)
-        .anyMatch(path -> Files.exists(path.resolve(exec)));
-    return existsInPath
+def hasRunbld() {
+    def path = ""
+    if (System.properties['os.name'].toLowerCase().contains('windows')) {
+        path = "where.exe runbld".execute().text
+    } else {
+        path = "which runbld".execute().text
+    }
+    return "" != path
 }
 
 def runbld(String... args) {
     try {
-        if(existsInPath("runbld")) {
-            def runbldProc = "runbld ${args.join(" ")}".execute()
+        if(hasRunbld()) {
+            def runbldProc = "runbld ${args.join(' ')}".execute()
             if( 0 == runbldProc.exitValue() ) {
                 return runbldProc.text
             } else {
                 // TODO: print the exit code, stderr/stdout
-                
+
             }
+        } else {
+            println "runbld not found, skipping..."
         }
     } catch(ex) {
         // Don't let runbld kill the build
@@ -30,9 +34,10 @@ def call(Closure body) {
         runbld("--build-id", buildId, "--event" "build-start")
         body()
     } finally {
-        sh "echo FINALLY"
+        println "echo FINALLY"
         def logfile = currentBuild.rawBuild.getLogFile()
         runbld("--build-id", buildId,
+               "--cwd", env.WORKSPACE,
                "--event", "build-end",
                "--result", currentBuild.currentResult,
                "--log-file", logfile.getAbsolutePath())
